9f728c7421a8e4b273056b9231fd8c7b7f96360e
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2024-04-26 20:11:57 -0300

    se quitaron los labels del slider

--- tp-react/src/components/Landing.tsx
+++ tp-react/src/components/Landing.tsx
@@ -10,26 +10,15 @@ export const Landing = () => {
                 <Carousel id="carrusel">
                     <Carousel.Item>
                         <img src="../../landingImg/guitarras_1.jpg" className="d-block w-100" />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
+
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src="../../landingImg/guitarras_2.jpg" className="d-block w-100" />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
+
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src="../../landingImg/guitarras_3.jpg" className="d-block w-100" />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                        </Carousel.Caption>
+
                     </Carousel.Item>
                 </Carousel>
             </section>
