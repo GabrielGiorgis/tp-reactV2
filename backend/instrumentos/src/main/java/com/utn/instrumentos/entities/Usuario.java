3f5c6b05f2b4a1d69bb454f5062e6950933c9b22
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-06-12 12:21:57 -0300

    Fix usuario seq

--- backend/instrumentos/src/main/java/com/utn/instrumentos/entities/Usuario.java
+++ backend/instrumentos/src/main/java/com/utn/instrumentos/entities/Usuario.java
@@ -13,7 +13,7 @@ import lombok.*;
 @Setter
 public class Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombreUsuario;
     private String contrasenia;
